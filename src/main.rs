aed81cdc8df4bb863f808f117f87429f430f479a
Simon Heimler <mail@simon-heimler.de>
2024-10-19 19:26:26 +0200

    Use microbit board library

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,30 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-extern crate panic_halt;
-
-use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use embedded_hal::digital::{OutputPin, PinState};
-use hal::{gpio::Level, pac};
-use nrf52833_hal as hal;
-
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{board::Board, hal::timer::Timer};
+use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    // Initialize RTT
     rtt_init_print!();
-
-    // Example usage of RTT for logging
     rprintln!("RTT is now enabled!");
 
-    let p = pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0);
-    let _col1 = port0.p0_28.into_push_pull_output(Level::Low);
-    let mut row1 = port0.p0_21.into_push_pull_output(Level::Low);
+    let mut board = Board::take().unwrap();
 
-    let mut is_on: bool = false;
+    let mut timer = Timer::new(board.TIMER1);
+
+    let _ = board.display_pins.col1.set_low();
+    let mut row1 = board.display_pins.row1;
+
     loop {
-        row1.set_state(PinState::from(is_on)).unwrap();
-        for _ in 0..100_000 {
-            nop();
-        }
-        is_on = !is_on;
-        rprintln!("PIN pin21: {}", is_on);
+        let _ = row1.set_low();
+        rprintln!("LED is OFF");
+        timer.delay_ms(1_000);
+        let _ = row1.set_high();
+        rprintln!("LED is ON");
+        timer.delay_ms(1_000);
     }
 }
